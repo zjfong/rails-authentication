0c9a1bb181f73fef674b8cf923908b07d1812b20
Zachary Fong <fong.zachary@gmail.com>
2016-10-04 17:21:53 -0700

    finish first rd auth

---
+++ user_auth/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
+end
---
+++ user_auth/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'users#index'
+
+  get '/users', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+end
